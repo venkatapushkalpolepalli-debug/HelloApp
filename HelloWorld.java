f45e1d8e90ada8c077d607461c47dfe1d0c3b7a8
venkatapushkalpolepalli-debug <venkatapushkalpolepalli@gmail.com>
2026-03-26 14:52:08 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        String name;
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello " + name);
+            name = args[0];
         } else {
-            System.out.println("Hello User");
+            name = "World";
         }
+        System.out.println("Hello " + name);
     }
 }
